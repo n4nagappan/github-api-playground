fe6196f7c6f0656b9e8484560d509ff57128f930
Nagappan <n4nagappan@gmail.com>
2016-01-12 18:02:50 +0000

    Re-structuring the html page

--- public/js/main.js
+++ public/js/main.js
@@ -1,18 +1,54 @@
 var editor;
+var endpoints = [
+    "/users",
+    "/users/:user",
+    "/repos/:owner/:repo/stargazers",
+    "/users/:username/starred",
+    "/users/:user",
+    "/repos/:owner/:repo/events",
+    "/repos/:owner/:repo/collaborators"
+];
+
 $(document).ready(function(){
+
+    //$('#urlbox').click(function(){
+    //    var pos = $("#urlbox").getCursorPosition();
+    //    var text = $("#urlbox").html();
+    //    console.log("pos : " + pos);
+    //    var start = text.
+    //    createSelection( document.getElementById('urlbox') , pos , pos+4);
+    //});
+
     $('#urlbox').keypress(function(e){
-        if(e.keyCode==13)
-        sendRequest();
+        if(e.keyCode==13){
+            var url = $("#urlbox").html();
+            sendRequest(url);
+        }
     });
+
     var myTextArea = document.getElementById('output');
     editor = CodeMirror.fromTextArea(myTextArea, {
         lineNumbers: true
     });
+
+    // populate endpoint divs
+    for( var i = 0 ; i < endpoints.length ; ++i ){
+        $('#left-panel').append('<div class="row fullwidth panel"> <a href="#" class="api">' + endpoints[i] + '</a></div>');
+    }
+
+    $(".api").click(function(e){
+        var url = $(e.target).html();
+        $('#urlbox').attr('value', url);
+    });
+
+    $("#execute").click(function(e){
+        var url = $("#urlbox").html();
+        sendRequest(url);
+    });
 });
 
-function sendRequest(){
-    var url = $('#urlbox').val();
-    console.log(url);
+function sendRequest(url){
+    console.log("url : " + url);
     $.get('/exec?url=' + url , function(data){
         var data = JSON.parse(data);
         editor.setValue(JSON.stringify(data, null, 2));
@@ -21,3 +57,38 @@ function sendRequest(){
         });
     });
 }
+
+(function($) {
+    $.fn.getCursorPosition = function() {
+        var input = this.get(0);
+        if (!input) return; // No (input) element found
+        if ('selectionStart' in input) {
+            // Standard-compliant browsers
+            return input.selectionStart;
+        } else if (document.selection) {
+            // IE
+            input.focus();
+            var sel = document.selection.createRange();
+            var selLen = document.selection.createRange().text.length;
+            sel.moveStart('character', -input.value.length);
+            return sel.text.length - selLen;
+        }
+    }
+})(jQuery);
+
+function createSelection(field, start, end) {
+    if( field.createTextRange ) {
+        var selRange = field.createTextRange();
+        selRange.collapse(true);
+        selRange.moveStart('character', start);
+        selRange.moveEnd('character', end);
+        selRange.select();
+    } else if( field.setSelectionRange ) {
+        field.setSelectionRange(start, end);
+    } else if( field.selectionStart ) {
+        field.selectionStart = start;
+        field.selectionEnd = end;
+    }
+    field.focus();
+}       
+
